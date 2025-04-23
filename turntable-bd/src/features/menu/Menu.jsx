3655bef36da55fea2f5a82afdb9800c52a733735
MA Ahi <bijoyrashidul21@gmail.com>
2025-04-23 16:02:45 +0600

    new update

--- turntable-bd/src/features/menu/Menu.jsx
+++ turntable-bd/src/features/menu/Menu.jsx
@@ -6,7 +6,7 @@ function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6 bg-gray-900 -mt-8">
+    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6  -mt-8">
       {menu.map((item) => (
         <MenuItem item={item} key={item.id} />
       ))}
