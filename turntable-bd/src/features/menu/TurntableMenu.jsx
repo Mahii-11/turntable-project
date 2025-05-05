7e662e3c3a9c05a3551e252a7ec1fe6ff90f813a
MA Ahi <bijoyrashidul21@gmail.com>
2025-05-05 21:19:13 +0600

    update

--- turntable-bd/src/features/menu/TurntableMenu.jsx
+++ turntable-bd/src/features/menu/TurntableMenu.jsx
@@ -1,17 +1,28 @@
 import { useLoaderData } from "react-router-dom";
 import { getTurntableParts } from "../../services/apiRestaurant";
 import TurntableItem from "./TurntableItem";
+import { Helmet } from "react-helmet";
 
 function TurntableMenu() {
   const menu = useLoaderData();
   console.log(menu);
 
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6  -mt-8">
-      {menu.map((item) => (
-        <TurntableItem item={item} key={item._id} />
-      ))}
-    </ul>
+    <>
+      <Helmet>
+        <title>Turntable Parts | BD Turntable</title>
+        <meta
+          name="description"
+          content="Explore high-quality turntable parts including belts, hinges, and tonearms at BD Turntable. Premium components for the best sound experience."
+        />
+      </Helmet>
+
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-6  -mt-8">
+        {menu.map((item) => (
+          <TurntableItem item={item} key={item._id} />
+        ))}
+      </ul>
+    </>
   );
 }
 
